9cc280816a20b4b298292ba2993fbda282c89a79
HamzaMateen <hamzamateen1583@gmail.com>
2024-05-23 03:01:24 +0500

    update backend a little, favicon, oauth in progress

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func main() {
-	fmt.Println("Hello, there bro!")
-	fmt.Println("Welcome to the backend of my personal website!")
+
+	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI("mongodb+srv://ycomb1nat0r:<password>@portfoliocluster.zv39tkh.mongodb.net/?retryWrites=true&w=majority&appName=PortfolioCluster").SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Send a ping to confirm a successful connection
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 }
